c041764663537a50f3556aceb7684759e8527855
Lea Verou <lea@verou.me>
2017-04-03 17:34:20 -0400

    Add demo on homepage

--- js/docs.js
+++ js/docs.js
@@ -1,30 +1,4 @@
-// Create the live examples
-$$(".example").forEach((example, i) => {
-	example.id = example.id || "example" + (i + 1);
-
-	if (!$("h1", example)) {
-		$.create("h1", {
-			textContent: "Example",
-			start: example
-		});
-	}
-
-	var code = $("script[type='text/plain']", example);
-
-	var container = $.create({
-		className: "example-container",
-		innerHTML: code.textContent,
-		after: code
-	});
-
-	var mavoRoot = $("[mv-app], [mv-storage]", container) || container;
-
-	mavoRoot.classList.add("debug-saving");
-	mavoRoot.setAttribute("mv-storage", mavoRoot.getAttribute("mv-storage") || "local");
-	mavoRoot.setAttribute("mv-app", mavoRoot.getAttribute("mv-app") || "");
-});
-
-// ???
+// Print out URL for empty non-local links
 $$('a[href^="#"]:empty').forEach(function (a) {
 	a.textContent = a.getAttribute("href").slice(1);
 });
---
+++ js/sitewide.js
@@ -0,0 +1,25 @@
+// Create the live examples
+$$(".example").forEach((example, i) => {
+	example.id = example.id || "example" + (i + 1);
+
+	if (!$("h1", example)) {
+		$.create("h1", {
+			textContent: "Example",
+			start: example
+		});
+	}
+
+	var code = $("script[type='text/plain']", example);
+
+	var container = $.create({
+		className: "example-container",
+		innerHTML: code.textContent,
+		after: code
+	});
+
+	var mavoRoot = $("[mv-app], [mv-storage]", container) || container;
+
+	mavoRoot.classList.add("debug-saving");
+	mavoRoot.setAttribute("mv-storage", mavoRoot.getAttribute("mv-storage") || "local");
+	mavoRoot.setAttribute("mv-app", mavoRoot.getAttribute("mv-app") || "");
+});
