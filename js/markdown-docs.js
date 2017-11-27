07e2590cafed54f66d53b5f2f7df63a9d3386403
Lea Verou <lea@verou.me>
2017-11-27 01:24:38 -0500

    Improve markdown docs code

--- js/markdown-docs.js
+++ js/markdown-docs.js
@@ -1,17 +1,69 @@
 (function() {
 
 Mavo.hooks.add("markdown-render-before", function(env) {
-	env.markdown = env.markdown.replace(/^(Note|Tip|Warning): /mi, function($0, $1) {
+	env.markdown = env.markdown.replace(/^(Note|Tip|Warning): /mig, function($0, $1) {
 		return "<p class=" + $1.toLowerCase() + ">";
 	});
 });
 
 document.addEventListener("mv-markdown-render", function(evt) {
 	// Create live demos
-	$$(`h2[id^=demo] + pre > code.language-markup,
-		h2[id^=demo] + p + pre > code.language-markup,
-		h2[id^=demo] + pre > code.language-html,
-			h2[id^=demo] + p + pre > code.language-html`, evt.target).forEach(createLiveDemo);
+	var s = Mavo.selectors;
+	var demoHeading = "h2[id^=demo], h6";
+	var selector = s.and(demoHeading, " + pre, + p + pre");
+	selector = s.and(selector, "> code.language-markup, > code.language-html")
+
+	$$(selector, evt.target).forEach(function createLiveDemo(code) {
+		var html = code.textContent;
+		var pre = code.parentNode;
+		var demoHTML = `<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="https://dev.mavo.io/dist/mavo.css" />
+<script src="https://dev.mavo.io/dist/mavo.js"></script>
+</head>
+<body>
+${html}
+</body>`;
+
+		var siblings = [], ps = pre;
+
+		do {
+			siblings.push(ps = ps.previousElementSibling);
+		} while (ps && !ps.matches(demoHeading));
+
+		var iframe = $.create("iframe", {
+			srcdoc: demoHTML,
+			onload: function(evt) {
+				updateIframeHeight();
+
+				iframe.contentDocument.addEventListener("mv-load", updateIframeHeight);
+			}
+		});
+
+		function updateIframeHeight() {
+			var h = iframe.contentDocument.documentElement.offsetHeight;
+			iframe.style.height = h + "px";
+		}
+
+		var container = $.create("section", {
+			className: "example side-by-side",
+			contents: [
+				{
+					className: "demo-container",
+					contents: [
+						pre,
+						{
+							className: "example-container",
+							contents: iframe
+						}
+					]
+				}
+			],
+			after: ps
+		});
+
+		$.start(siblings, container);
+	});
 
 	// Highlight code areas
 	requestAnimationFrame(() => {
@@ -24,38 +76,5 @@ document.addEventListener("mv-markdown-render", function(evt) {
 		});
 	});
 });
-
-function createLiveDemo(code) {
-	var markup = code.textContent;
-	var pre = code.parentNode;
-
-	var iframe = $.create("iframe", {
-		srcdoc: `<!DOCTYPE html>
-<head>
-<link rel="stylesheet" href="https://dev.mavo.io/dist/mavo.css" />
-<script src="https://dev.mavo.io/dist/mavo.js"></script>
-</head>
-<body>
-${markup}
-</body>
-`
-	});
-
-	$.create({
-		className: "example side-by-side",
-		around: pre
-	});
-
-	$.create({
-		className: "demo-container",
-		around: pre
-	});
-
-	$.create({
-		className: "example-container",
-		contents: iframe,
-		after: pre
-	});
-}
 
 })();
